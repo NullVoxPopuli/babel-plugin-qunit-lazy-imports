a40225b79923c2091f1cbcef977521ffdf7adb8f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-17 18:40:46 -0400

    Cleanup

--- test-app/tests/test-helper.js
+++ test-app/tests/test-helper.js
@@ -1,12 +1,16 @@
-import Application from 'test-app/app';
-import config from 'test-app/config/environment';
+import Application from '#app/app.js';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
 
 export function start() {
-  setApplication(Application.create(config.APP));
+  setApplication(
+    Application.create({
+      autoboot: false,
+      element: '#ember-testing',
+    })
+  );
 
   setup(QUnit.assert);
   setupEmberOnerrorValidation();
