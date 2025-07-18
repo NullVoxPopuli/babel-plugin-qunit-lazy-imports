0f26fc743233ba94ca15e41d3616550a3909d0a9
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-18 18:13:18 -0400

    Another failure

--- tests/missing-hooks.test.js
+++ tests/missing-hooks.test.js
@@ -82,3 +82,47 @@ it("inserts the hooks.before when arrow", () => {
     });"
   `);
 });
+
+
+it("avoids 'redeclaration of const'", () => {
+  expect(
+    transform(
+      `
+      import { visit, currentURL } from '@ember/test-helpers';
+      import { module, test } from 'qunit';
+      import someFancyThing from 'fancy-app/some/path';
+
+      module('Acceptance | test', function () {
+        test('should work', async function (assert) {
+          const hooks = '/whatever';
+          await visit(hooks);
+          assert.strictEqual(currentURL(), '/');
+          console.log(someFancyThing)
+        });
+      });
+`,
+      {
+        startsWith: ["fancy-app/"],
+      },
+    ),
+  ).toMatchInlineSnapshot(`
+    "import { visit, currentURL } from '@ember/test-helpers';
+    import { module, test } from 'qunit';
+    let someFancyThing;
+    module('Acceptance | test', function (hooks2) {
+      hooks2.before(async () => {
+        await Promise.all([(async () => {
+          let module = await import('fancy-app/some/path');
+          someFancyThing = module.default;
+        })()]);
+      });
+      test('should work', async function (assert) {
+        const hooks = '/whatever';
+        await visit(hooks);
+        assert.strictEqual(currentURL(), '/');
+        console.log(someFancyThing);
+      });
+    });"
+  `);
+});
+
