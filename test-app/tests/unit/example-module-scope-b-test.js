df396b3f01ab2c7240cb5a0b5b03704e49153587
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-08-29 14:35:28 -0400

    More tests

---
+++ test-app/tests/unit/example-module-scope-b-test.js
@@ -0,0 +1,19 @@
+import QUnit, { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+import { numbers } from '../utils.js';
+
+import { numbers as n } from 'test-app/tests/utils';
+
+function assertOne() {
+  QUnit.assert.strictEqual(n.one, 1);
+  QUnit.assert.strictEqual(numbers.one, 1);
+}
+
+module('Unit module-scope-b', function (hooks) {
+  setupTest(hooks);
+
+  test('working', function () {
+    assertOne();
+  });
+});
